f6e1141093b5dafd600d1b86f44a312d8ea51f27
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-04-09 22:56:14 -0400

    User Ross Edman saved changes to main.py at 04/10/2024, 02:56:14 UTC time.

--- 9a72dc24-72a2-4a77-a19a-d1ef16bb5433/main.py
+++ 9a72dc24-72a2-4a77-a19a-d1ef16bb5433/main.py
@@ -16,17 +16,18 @@ class TradingStrategy(Strategy):
    def run(self, data):
       holdings = data["holdings"]
       data = data["ohlcv"]
-      
-      allocation_dict = {}
-      rsi_dict = {}
-      for ticker in self.assets:
-         try: rsi_dict[ticker] = RSI(ticker, data, 14)[-1]
-         except: rsi_dict[ticker] = 1
 
-      log(rsi_dict)
+      log(str(data))
+      
+      # allocation_dict = {}
+      # rsi_dict = {}
+      # for ticker in self.assets:
+      #    try: rsi_dict[ticker] = RSI(ticker, data, 14)[-1]
+      #    except: rsi_dict[ticker] = 1
 
       # allocation_dict = {i: rsi_dict[i]/(sum(rsi_dict.values())+10) for i in self.assets}
       # for key in allocation_dict:
       #    if abs(allocation_dict[key]-holdings.get(key, 0))>0.02:
       #       return TargetAllocation(allocation_dict)
+
       return None
