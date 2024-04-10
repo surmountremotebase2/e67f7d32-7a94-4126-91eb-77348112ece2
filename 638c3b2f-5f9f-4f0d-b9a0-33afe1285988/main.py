d659e535a1ca18ffb964da67b922d8be5caee471
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-04-09 23:45:35 -0400

    User Ross Edman saved changes to main.py at 04/10/2024, 03:45:34 UTC time.

--- 638c3b2f-5f9f-4f0d-b9a0-33afe1285988/main.py
+++ 638c3b2f-5f9f-4f0d-b9a0-33afe1285988/main.py
@@ -16,12 +16,17 @@ class TradingStrategy(Strategy):
 
     def run(self, data):
         d = data["ohlcv"]
+        allocation_dict = {}
         for i in self.tickers:
-            # get yesterday's price at close
             current_price = d[-1][i]["close"]
             this_sma = SMA(i, d, 50)
             this_rsi = RSI(i, d, 14)
             if current_price > this_sma[-1]:
-                log("trade!")
+                allocation_dict.append({i: 1})
 
-        return TargetAllocation({})
+        # self.count += 1
+        # if (self.count % 30 == 1):
+        #     allocation_dict = {self.tickers[i]: self.weights[i]/sum(self.weights) for i in range(len(self.tickers))}
+        #     return TargetAllocation(allocation_dict)
+
+        return TargetAllocation(allocation_dict)
