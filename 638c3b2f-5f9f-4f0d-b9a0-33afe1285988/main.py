84f7144487265ec65db519a1576350c316f0549f
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-04-09 23:58:10 -0400

    User Ross Edman saved changes to main.py at 04/10/2024, 03:58:09 UTC time.

--- 638c3b2f-5f9f-4f0d-b9a0-33afe1285988/main.py
+++ 638c3b2f-5f9f-4f0d-b9a0-33afe1285988/main.py
@@ -14,17 +14,30 @@ class TradingStrategy(Strategy):
     def interval(self):
         return "1day"
 
+    def has_momentum(self, ticker, data):
+        '''
+        check if the ticker provided has 3 days of closes consecutively 
+        above the previous days, this shows some sort of momentum
+        '''
+        d = data["ohlcv"]
+        return d[-1][ticker]["close"] > d[-2][ticker]["close"] > d[-3][ticker]["close"]
+
     def run(self, data):
         d = data["ohlcv"]
         allocation_dict = {}
-        for i in self.tickers:
-            current_price = d[-1][i]["close"]
-            fifty_day_sma = SMA(i, d, 50)
-            two_hundy_day_sma = SMA(i, d, 200)
-            if (current_price > fifty_day_sma[-1]) and (fifty_day_sma[-1] > two_hundy_day_sma[-1]):
-                allocation_dict[i] = 1
 
-            if (current_price < fifty_day_sma[-1] < two_hundy_day_sma[-1]):
-                allocation_dict[i] = 0
+        for i in self.tickers:
+            if has_momentum(i, d):
+                log("MOMENTUM!")
+
+        # for i in self.tickers:
+        #     current_price = d[-1][i]["close"]
+        #     fifty_day_sma = SMA(i, d, 50)
+        #     two_hundy_day_sma = SMA(i, d, 200)
+        #     if (current_price > fifty_day_sma[-1]) and (fifty_day_sma[-1] > two_hundy_day_sma[-1]):
+        #         allocation_dict[i] = 1
+
+        #     if (current_price < fifty_day_sma[-1] < two_hundy_day_sma[-1]):
+        #         allocation_dict[i] = 0
 
         return TargetAllocation(allocation_dict)
