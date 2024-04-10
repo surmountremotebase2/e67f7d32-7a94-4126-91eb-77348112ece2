32cd43aa6fcb55c8f81bf8e5f4cd3778c8305e09
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-04-09 23:23:02 -0400

    User Ross Edman saved changes to main.py at 04/10/2024, 03:23:01 UTC time.

--- 638c3b2f-5f9f-4f0d-b9a0-33afe1285988/main.py
+++ 638c3b2f-5f9f-4f0d-b9a0-33afe1285988/main.py
@@ -21,8 +21,8 @@ class TradingStrategy(Strategy):
             this_rsi = RSI(i, d, 14)
 
             if this_rsi[0] <= 30:
-                if this_rsi[-1] < this_rsi[0]:
-                    log("trade")
+                #if this_rsi[-1] < this_rsi[0]:
+                log("trade")
 
             # log(str(this_rsi[-1]))
             # log(str(this_rsi[-2]))
