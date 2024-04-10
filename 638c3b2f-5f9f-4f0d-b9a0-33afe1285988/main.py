80268d21b125105a65ad72b24e760b121a91040b
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-04-09 23:01:18 -0400

    User Ross Edman saved changes to main.py at 04/10/2024, 03:01:17 UTC time.

--- 638c3b2f-5f9f-4f0d-b9a0-33afe1285988/main.py
+++ 638c3b2f-5f9f-4f0d-b9a0-33afe1285988/main.py
@@ -1,1 +1,29 @@
-#Type code here
+from surmount.base_class import Strategy, TargetAllocation
+from surmount.technical_indicators import RSI, EMA, SMA, MACD, MFI, BB
+from surmount.logging import log
+
+class TradingStrategy(Strategy):
+    def __init__(self):
+        self.tickers = ["SPY", "QQQ"\]
+
+    @property
+    def assets(self):
+        return self.tickers
+
+    @property
+    def interval(self):
+        return "1hour"
+
+    def run(self, data):
+        d = data["ohlcv"]
+        stake = 0
+        # if len(d)>3 and "13:00" in d[-1]["QQQ"]["date"]:
+        #     v_shape = d[-2]["QQQ"]["close"]<d[-3]["QQQ"]["close"] and d[-1]["QQQ"]["close"]>d[-2]["QQQ"]["close"]
+        #     log(str(v_shape))
+        #     if v_shape:
+        #         stake = 1
+
+        for i in self.assets:
+            log(i)
+
+        return TargetAllocation({"SPY": stake})
