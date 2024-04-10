a653e634481a85b21403a00f9f3173159eeeafa4
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-04-09 23:49:37 -0400

    User Ross Edman saved changes to main.py at 04/10/2024, 03:49:36 UTC time.

--- 638c3b2f-5f9f-4f0d-b9a0-33afe1285988/main.py
+++ 638c3b2f-5f9f-4f0d-b9a0-33afe1285988/main.py
@@ -21,7 +21,7 @@ class TradingStrategy(Strategy):
             current_price = d[-1][i]["close"]
             fifty_day_sma = SMA(i, d, 50)
             two_hundy_day_sma = SMA(i, d, 200)
-            if (current_price > fifty_day_sma[-1]) and (fifty_day_sma[-1] > two_hundy_day_sma):
+            if (current_price > fifty_day_sma[-1]) and (fifty_day_sma[-1] > two_hundy_day_sma[-1]):
                 allocation_dict[i] = 1
 
         return TargetAllocation(allocation_dict)
