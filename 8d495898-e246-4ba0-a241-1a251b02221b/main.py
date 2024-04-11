29000d802f7e5e52a0449d93234e91bac9d4f074
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-04-10 21:43:50 -0400

    User Ross Edman saved changes to main.py at 04/11/2024, 01:43:49 UTC time.

--- 8d495898-e246-4ba0-a241-1a251b02221b/main.py
+++ 8d495898-e246-4ba0-a241-1a251b02221b/main.py
@@ -30,7 +30,8 @@ class TradingStrategy(Strategy):
         ema_fast = EMA(ticker, data, 7)
         ema_slow = EMA(ticker, data, 14)
         return (ema_fast[-1] < ema_slow[-1]) and \
-               (ema_fast[-2] > ema_slow[-2]) 
+               (ema_fast[-2] > ema_slow[-2]) and \
+               (ema_fast[-3]) > ema_slow[-3])
 
     def run(self, data):
         d = data["ohlcv"]
