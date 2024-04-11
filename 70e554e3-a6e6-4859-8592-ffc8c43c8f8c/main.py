c73e2f4f777601acc42751bf3ca970c1eb696cab
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-04-11 17:39:09 -0400

    User Ross Edman saved changes to main.py at 04/11/2024, 21:39:08 UTC time.

--- 70e554e3-a6e6-4859-8592-ffc8c43c8f8c/main.py
+++ 70e554e3-a6e6-4859-8592-ffc8c43c8f8c/main.py
@@ -14,12 +14,26 @@ class TradingStrategy(Strategy):
     def interval(self):
         return "4hour"
 
+    def has_momentum(self, ticker, data):
+        '''
+        check if the ticker provided has 3 days of closes consecutively 
+        above the previous days, this shows some sort of momentum
+        '''
+        return data[-1][ticker]["close"] > \
+               data[-2][ticker]["close"] > \
+               data[-3][ticker]["close"]
+
     def run(self, data):
         d = data["ohlcv"]
         allocation_dict = {}
         for i in self.tickers:
             bb = BB(i, d, 14, 1)
+            rsi = RSI(i, d, 14)
 
-            if bb['lower'][-1] 
+            if (d[-1][i]["close"] > bb['lower'][-1]) and (rsi[-1] >= 50):
+                allocation_dict = {i: 1}
+
+            if (d[-1][i][closer] < bb['lower'][-1]):
+                allocation_dict = {i: 0}
 
         return TargetAllocation(allocation_dict)
