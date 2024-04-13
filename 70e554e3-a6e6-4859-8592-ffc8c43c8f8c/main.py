32bfa6aff198891096a69e6abe80858ef23c1ee5
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-04-13 01:50:13 -0400

    User Ross Edman saved changes to main.py at 04/13/2024, 05:50:12 UTC time.

--- 70e554e3-a6e6-4859-8592-ffc8c43c8f8c/main.py
+++ 70e554e3-a6e6-4859-8592-ffc8c43c8f8c/main.py
@@ -4,7 +4,7 @@ from surmount.logging import log
 
 class TradingStrategy(Strategy):
     def __init__(self):
-        self.tickers = ["QQQ"]
+        self.tickers = ["SPXL"]
 
     @property
     def assets(self):
