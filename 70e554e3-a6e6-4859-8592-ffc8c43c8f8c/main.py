20d339aa67ae81011df0c649265297edd88c4df7
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-04-13 01:44:59 -0400

    User Ross Edman saved changes to main.py at 04/13/2024, 05:44:59 UTC time.

--- 70e554e3-a6e6-4859-8592-ffc8c43c8f8c/main.py
+++ 70e554e3-a6e6-4859-8592-ffc8c43c8f8c/main.py
@@ -14,25 +14,8 @@ class TradingStrategy(Strategy):
     def interval(self):
         return "1day"
 
-    def has_resistance(self, ticker, data):
-        bb = BB(i, d, 14, 1)
-        return bb['mid'][-1] > data[-1][ticker]['open'] and \
-               bb['mid'][-1] > data[-1][ticker]['close'] and \
-               bb['mid'][-1] < data[-1][ticker]['high']
-
-    def has_rising_volume(self, ticker, data):
-        return data[-1][ticker]['volume'] > data[-2][ticker]['volume'] > data[-3][ticker]['volume']
-
     def has_falling_volume(self, ticker, data):
         return data[-1][ticker]['volume'] < data[-2][ticker]['volume'] < data[-3][ticker]['volume']
-
-    def has_rising_rsi(self, ticker, data):
-        rsi = RSI(ticker, data, 14)
-        return (rsi[-1] >= 50) and (rsi[-1] > rsi[-2] > rsi[-3])
-
-    def has_falling_rsi(self, ticker, data):
-        rsi = RSI(ticker, data, 14)
-        return (rsi[-1] < rsi[-2] < rsi[-3])
 
     def run(self, data):
         d = data["ohlcv"]
@@ -40,10 +23,6 @@ class TradingStrategy(Strategy):
         for i in self.tickers:
             bb  = BB(i, d, 14, 1)
             rsi = RSI(i, d, 14)
-
-            current_price_low   = d[-1][i]['low']
-            current_price_high  = d[-1][i]['high']
-            current_price_open  = d[-1][i]['open']
             current_price_close = d[-1][i]['close']
             
             # entrace
