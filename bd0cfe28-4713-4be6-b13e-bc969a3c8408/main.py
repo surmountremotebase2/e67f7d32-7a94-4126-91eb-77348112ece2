f2be306178ace29845978d960f56c0397f60430a
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-04-14 17:31:43 -0400

    User Ross Edman saved changes to main.py at 04/14/2024, 21:31:43 UTC time.

--- bd0cfe28-4713-4be6-b13e-bc969a3c8408/main.py
+++ bd0cfe28-4713-4be6-b13e-bc969a3c8408/main.py
@@ -37,6 +37,13 @@ class TradingStrategy(Strategy):
                         allocation_dict = {i: min(0, h[i] - 0.2)}
                     else:
                         allocation_dict = {i: 0}
+            elif (current_price_close >= bb['upper'][-1]):
+                if i in h:
+                    if h[i] >= 0.2:
+                        allocation_dict = {i: min(0, h[i] - 0.2)}
+                    else:
+                        allocation_dict = {i: 0}
+
             # else:
             #     log("empty")
 
