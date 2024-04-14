36722cdec2d3ff43f45aaec04da434764b47a04d
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-04-14 13:30:52 -0400

    User Ross Edman saved changes to main.py at 04/14/2024, 17:30:51 UTC time.

--- bd0cfe28-4713-4be6-b13e-bc969a3c8408/main.py
+++ bd0cfe28-4713-4be6-b13e-bc969a3c8408/main.py
@@ -30,13 +30,13 @@ class TradingStrategy(Strategy):
             if (current_price_close >= bb['mid'][-1]) and rsi[-1] >= 50:
                 #if h[i] >= 0:
                 log("buy")
-                allocation_dict = {i: min(0.1, h[i]+0.1)}
+                allocation_dict = {i: min(1, h[i]+0.1)}
                 # else:
                 #     allocation_dict = {i: 0.25}
             elif (current_price_close > bb['upper'][-1]):
                 log("sell")
                 if h[i] > 0:
-                    allocation_dict = {i: min(0.1, h[i]-0.1)}
+                    allocation_dict = {i: min(1, h[i]-0.1)}
                 else:
                     allocation_dict = {i: 0}
             else:
