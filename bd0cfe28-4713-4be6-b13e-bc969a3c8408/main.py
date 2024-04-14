b4fe3edc137f5e6e5d01d0d15452ac41ddf88770
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-04-14 13:21:57 -0400

    User Ross Edman saved changes to main.py at 04/14/2024, 17:21:57 UTC time.

--- bd0cfe28-4713-4be6-b13e-bc969a3c8408/main.py
+++ bd0cfe28-4713-4be6-b13e-bc969a3c8408/main.py
@@ -14,8 +14,8 @@ class TradingStrategy(Strategy):
     def interval(self):
         return "1day"
 
-    def has_falling_volume(self, ticker, data):
-        return data[-1][ticker]['volume'] < data[-2][ticker]['volume'] < data[-3][ticker]['volume']
+    # def has_falling_volume(self, ticker, data):
+    #     return data[-1][ticker]['volume'] < data[-2][ticker]['volume'] < data[-3][ticker]['volume']
 
     def run(self, data):
         d = data["ohlcv"]
@@ -26,20 +26,17 @@ class TradingStrategy(Strategy):
             rsi = RSI(i, d, 2)
             current_price_close = d[-1][i]['close']
             
-            # entrace
-            #
-            # if the current price opened below middle bollinger band and closed above
-            # middle bollinger band and also has rising RSI above 50, buy!
-            # if (current_price_close > bb['mid'][-1]) \
-            #   and self.has_rising_rsi(i, d):
-            #     allocation_dict = {i: 1}
             if (current_price_close > bb['mid'][-1]) and rsi[-1] >= 50:
-                allocation_dict = {i: 1}
-            
-            # exit
-            #
-            # exit position when it closes below the lower bollinger band
-            if (current_price_close < bb['mid'][-1]) and self.has_falling_volume(i, d):
+                if holdings[i] >= 0:
+                    allocation_dict = {i: min(1, h[i]+0.1)}
+                else:
+                    allocation_dict = {i: 0.4}
+            elif (current_price_close > bb['upper'][-1]):
+                if holdings[i] > 0:
+                    allocation_dict = {i: min(1, h[i]-0.1)}
+                else:
+                    allocation_dict = {i: 0}
+            else:
                 allocation_dict = {i: 0}
 
         return TargetAllocation(allocation_dict)
