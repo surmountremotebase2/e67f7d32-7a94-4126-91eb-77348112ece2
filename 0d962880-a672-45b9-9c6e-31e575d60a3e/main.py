69da4e69b0033df87b0a50c97c1f7ea5a94d89e0
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-04-15 08:56:03 -0400

    User Ross Edman saved changes to main.py at 04/15/2024, 12:56:02 UTC time.

--- 0d962880-a672-45b9-9c6e-31e575d60a3e/main.py
+++ 0d962880-a672-45b9-9c6e-31e575d60a3e/main.py
@@ -1,6 +1,8 @@
 from surmount.base_class import Strategy, TargetAllocation
 from surmount.technical_indicators import RSI, EMA, SMA, MACD, MFI, BB
 from surmount.logging import log
+import pandas_ta as ta
+import pandas as pd
 
 class TradingStrategy(Strategy):
     def __init__(self):
@@ -14,42 +16,33 @@ class TradingStrategy(Strategy):
     def interval(self):
         return "1day"
 
-    def has_falling_volume(self, ticker, data):
-        return data[-1][ticker]['volume'] < data[-2][ticker]['volume'] < data[-3][ticker]['volume']
+    def SMAVol(ticker, data, length):
+        '''Calculate the moving average of trading volume
+
+        :param ticker: a string ticker
+        :param data: data as provided from the OHLCV data function
+        :param length: the window
+
+        :return: list with float SMA
+        '''
+        close = [i[ticker]["volume"] for i in data]
+        d = ta.sma(pd.Series(close), length=length)
+        if d is None:
+            return None
+        return d.tolist()
 
     def run(self, data):
         d = data["ohlcv"]
         h = data["holdings"]
         allocation_dict = {} 
         for i in self.tickers:
-            bb  = BB(i, d, 14, 1.3)
-            rsi = RSI(i, d, 5)
-            macd = MACD(i, d, 12, 26)
+            current_price_open  = d[-1][i]['open']
             current_price_close = d[-1][i]['close']
 
-            log(str(macd[-1]))
+            vol_sma_fast = SMAVol(i, d, 5)
+            vol_sma_slow = SMAVol(i, d, 12)
 
-            # enter a position if the price closes above the middle of the 
-            # bollinger band and has an RSI of 50 or greater. add the position
-            # incrementally.
-            # if (current_price_close >= bb['mid'][-1]) and rsi[-1] >= 50:
-            #     if i in h:
-            #         allocation_dict = {i: min(1, h[i]+0.1)}
-            #     else:
-            #         allocation_dict = {i: 0.1}
-            # take profits, sell all if closes above upper bollinger band and has 
-            # an RSI of 70 or greater
-            # elif (current_price_close >= bb['upper'][-1]) and rsi[-1] >= 70:
-            #     allocation_dict = {i: 0}
-            # exit position, exit if closes below the lower bollinger band and has falling
-            # volume, this attempts to exit the position incrementally but in 20% increments
-            # elif (current_price_close <= bb['lower'][-1]) and self.has_falling_volume(i, d):
-            #     if i in h:
-            #         if h[i] >= 0.1:
-            #             allocation_dict = {i: min(0, h[i] - 0.1)}
-            #         else:
-            #             allocation_dict = {i: 0}
-            # else:
-            #     allocation_dict = h
+            if current_price_close > vol_sma_fast[-1] > vol_sma_slow[-1]:
+                allocation_dict = {i: 1}
 
         return TargetAllocation(allocation_dict)
