0e5a0be6992ae29578192967f4025318edeb90a3
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-04-17 00:07:07 -0400

    User Ross Edman saved changes to main.py at 04/17/2024, 04:07:06 UTC time.

--- c03dfb6c-4fc3-4dbf-9b8d-67c3ef8260fc/main.py
+++ c03dfb6c-4fc3-4dbf-9b8d-67c3ef8260fc/main.py
@@ -45,7 +45,8 @@ class TradingStrategy(Strategy):
             if (vol_sma_fast[-1] > vol_sma_slow[-1]) and rsi[-1] >= 50:
                 allocation_dict = {i: 1}
 
-            if (vol_sma_fast[-1] < vol_sma_slow[-1]):
+            #if (vol_sma_fast[-1] < vol_sma_slow[-1]):
+            if rsi[-1] < 50:
                 allocation_dict = {i: 0}
 
         return TargetAllocation(allocation_dict)
