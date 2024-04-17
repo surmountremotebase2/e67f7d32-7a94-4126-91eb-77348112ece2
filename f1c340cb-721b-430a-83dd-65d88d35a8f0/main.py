eeb123b5e105d7713e4d5a3008b72ae87132703b
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-04-17 00:19:39 -0400

    Clone from SOXX (Volume SMA) (0d962880-a672-45b9-9c6e-31e575d60a3e)

---
+++ f1c340cb-721b-430a-83dd-65d88d35a8f0/main.py
@@ -0,0 +1,51 @@
+from surmount.base_class import Strategy, TargetAllocation
+from surmount.technical_indicators import RSI, EMA, SMA, MACD, MFI, BB
+from surmount.logging import log
+import pandas_ta as ta
+import pandas as pd
+
+class TradingStrategy(Strategy):
+    def __init__(self):
+        self.tickers = ["SOXX"]
+
+    @property
+    def assets(self):
+        return self.tickers
+
+    @property
+    def interval(self):
+        return "1day"
+
+    def SMAVol(self, ticker, data, length):
+        '''Calculate the moving average of trading volume
+
+        :param ticker: a string ticker
+        :param data: data as provided from the OHLCV data function
+        :param length: the window
+
+        :return: list with float SMA
+        '''
+        close = [i[ticker]["volume"] for i in data]
+        d = ta.sma(pd.Series(close), length=length)
+        if d is None:
+            return None
+        return d.tolist()
+
+    def run(self, data):
+        d = data["ohlcv"]
+        h = data["holdings"]
+        allocation_dict = {} 
+        for i in self.tickers:
+            current_price_open  = d[-1][i]['open']
+            current_price_close = d[-1][i]['close']
+
+            vol_sma_fast = self.SMAVol(i, d, 5)
+            vol_sma_slow = self.SMAVol(i, d, 12)
+
+            if vol_sma_fast[-1] > vol_sma_slow[-1]:
+                allocation_dict = {i: 1}
+
+            if vol_sma_fast[-1] < vol_sma_slow[-1]:
+                allocation_dict = {i: 0}
+
+        return TargetAllocation(allocation_dict)
